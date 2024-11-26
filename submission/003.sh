a9db7b5b77cc51c9e39567a2bed0f738240f7bc3
Michel Diz <MichelDiz@users.noreply.github.com>
2024-11-26 18:59:04 -0300

    add a terceira

--- submission/003.sh
+++ submission/003.sh
@@ -1,1 +1,21 @@
+#!/bin/bash
+
 # How many new outputs were created by block 123,456?
+
+pathconf="-conf=/Users/micheldiz/.bitcoin/bitcoin.conf"
+
+
+block_hash=$(bitcoin-cli $pathconf getblockhash 123456)
+txns=$(bitcoin-cli $pathconf getblock $block_hash | jq -r '.tx[]')
+
+total_outputs=0
+
+for tx in $txns; do
+  outputs=$(bitcoin-cli $pathconf getrawtransaction $tx 1 | jq '.vout | length')
+  total_outputs=$((total_outputs + outputs))
+done
+
+# Exibir o total de saídas
+echo $total_outputs
+
+#24
