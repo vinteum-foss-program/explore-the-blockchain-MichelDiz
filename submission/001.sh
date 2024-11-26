06a283d0ec17af5ca9dd3363dffaa53ea97d604c
Michel Diz <MichelDiz@users.noreply.github.com>
2024-11-26 18:58:41 -0300

    add a primeira

--- submission/001.sh
+++ submission/001.sh
@@ -1,1 +1,5 @@
+#!/bin/bash
 # What is the hash of block 654,321?
+bitcoin-cli -conf=/Users/micheldiz/.bitcoin/bitcoin.conf getblockhash 654321
+
+#000000000000000000058452bbe379ad4364fe8fda68c45e299979b492858095
