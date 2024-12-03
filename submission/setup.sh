b124c8da9ababa605e2897ea101640a6aacd41cb
Michel Diz <MichelDiz@users.noreply.github.com>
2024-12-03 12:11:00 -0300

    remove comments and add EOF

--- submission/setup.sh
+++ submission/setup.sh
@@ -1,13 +1,6 @@
-# wget https://bitcoincore.org/bin/bitcoin-core-27.1/bitcoin-27.1-arm64-apple-darwin.tar.gz
-# tar -xzvf bitcoin-27.1-arm64-apple-darwin.tar.gz
-# sudo cp bitcoin-27.1/bin/* /usr/local/bin/
-# brew install wget
-# brew install jq
-
 wget https://bitcoincore.org/bin/bitcoin-core-27.1/bitcoin-27.1-x86_64-linux-gnu.tar.gz
 tar -xzvf bitcoin-27.1-x86_64-linux-gnu.tar.gz
 ln -s $PWD/bitcoin-27.1/bin/* /usr/local/bin/
-mkdir -p ~/.bitcoin
 
 mkdir -p ~/.bitcoin
 echo "rpcconnect=84.247.182.145" >> ~/.bitcoin/bitcoin.conf
